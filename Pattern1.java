39cfba81c0ed55223eb2c22660fce987714a41dc
Rumaisha02 <134322056+Rumaisha02@users.noreply.github.com>
2025-03-06 17:25:50 +0000

    done

--- Pattern1.java
+++ Pattern1.java
@@ -45,26 +45,53 @@ public class Pattern1 {
     //         sc.close();
     // }
 
+    // public static void main(String[] args){
+    //         Scanner sc=new Scanner (System.in);
+    //             System.out.println("Enter n");
+    //             int n=sc.nextInt();
+    //             for(int i=1;i<=n;i++){
+    //               /*
+    //                *   //alternative way is..
+    //                * if i+j=even  PRINT 1
+    //                * if i+j=odd PRINT 0
+    //                */
+    //                for(int j=1;j<=i;j++){
+    //                 if(i==j){
+    //                     System.out.print("1" + " ");
+    //                 }
+    //                 else if((i%2!=0 && j%2==0) || (i%2==0 && j%2!=0)){
+    //                    System.out.print("0" + " ");
+    //                 } 
+    //                 else{
+    //                     System.out.print("1" + " ");
+    //                 }
+    //             }
+    //                 System.out.println();
+    //             }
+    //   sc.close();
+    //  }
+
     public static void main(String[] args){
-            Scanner sc=new Scanner (System.in);
-                System.out.println("Enter n");
-                int n=sc.nextInt();
-                for(int i=1;i<=n;i++){
-                   for(int j=1;j<=i;j++){
-                    if(i==j){
-                        System.out.print("1" + " ");
+        Scanner sc=new Scanner (System.in);
+            System.out.println("Enter n");
+            int n=sc.nextInt();
+            for (int i = 0; i <=n; i++) {
+                 for (int j = 0; j <=n; j++) {
+                    if(i+j==5 || i+j>5){
+                        System.out.print("*"+ " ");
+                    }else{
+                        System.out.print(" ");
                     }
-                    else if((i%2!=0 && j%2==0) || (i%2==0 && j%2!=0)){
-                       System.out.print("0" + " ");
-                    } 
-                    else{
-                        System.out.print("1" + " ");
-                    }
-                }
-                    System.out.println();
-                }
-      sc.close();
-     }
+                 }
+                 for (int k = n; k >=i; k--) {
+                    System.out.print("*" + " ");
+                 }
+                 System.out.println();
+            }
+
+            sc.close();
+        }
+
 }
 
 
