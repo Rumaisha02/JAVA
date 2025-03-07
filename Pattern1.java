68d0076ed669827948a2bc130b7621d8280070cb
Rumaisha02 <134322056+Rumaisha02@users.noreply.github.com>
2025-03-07 18:35:47 +0000

    not done yet

--- Pattern1.java
+++ Pattern1.java
@@ -113,25 +113,55 @@ public class Pattern1 {
     //     }
 
 
+    // public static void main(String[] args){
+    //     Scanner sc=new Scanner (System.in);
+    //         System.out.println("Enter n");
+    //         int n=sc.nextInt();
+    //         for (int i =1; i <=n; i++) {
+    //             for (int j =1; j <=n; j++) {
+    //                 if(i+j>n ){
+    //                     int p=i;
+    //                     System.out.print((p) + " ");
+    //                 }else{
+    //                     System.out.print(" ");
+    //                 }
+    //             }
+
+    //             // for (int k = 2; k <=i; k++) {
+    //             //     System.out.print(k + " ");
+    //             // }
+    //             System.out.println();
+    //         }
+    //         sc.close();
+    //     }
+
+
+
     public static void main(String[] args){
         Scanner sc=new Scanner (System.in);
             System.out.println("Enter n");
-            int n=sc.nextInt();
-            for (int i =1; i <=n; i++) {
-                for (int j =1; j <=n; j++) {
-                    if(i+j>n ){
-                        int p=i;
-                        System.out.print((p) + " ");
+            int n=sc.nextInt();  
+            for(int i=1;i<=n;i++){
+                // for(int j=1;j<=i;j++){
+                //     System.out.print("*" + " ");
+                // }
+
+                for(int k=1;k<=n;k++){
+                    if(i+k>=n){
+                        System.out.print("*"+" ");
                     }else{
                         System.out.print(" ");
                     }
                 }
-
-                // for (int k = 2; k <=i; k++) {
-                //     System.out.print(k + " ");
-                // }
                 System.out.println();
             }
+            
+            
+            
+            
+            
+            
+            
             sc.close();
         }
 }
