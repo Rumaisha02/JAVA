05dedfa0a4960aa1102091ff2cdba59b972693ee
Rumaisha02 <134322056+Rumaisha02@users.noreply.github.com>
2025-07-10 17:27:54 +0000

    ..

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,21 +1,24 @@
+import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        int[] arr = {122,45,76,44,312,87,5542,24};
         
         System.out.println("Unsorted array:");
-        printArray(arr);
+        System.out.println(Arrays.toString(arr));
         
         bubbleSort(arr);
         
         System.out.println("\nSorted array:");
-        printArray(arr);
+        System.out.println(Arrays.toString(arr));
+
     }
 
     // Bubble Sort method
     public static void bubbleSort(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
+            int swap=0;//adding this element so that when the array is already sorted..it doesn't run the whole loop and comes out of it if swapped element is stil
             // Last i elements are already sorted, no need to check them
             for (int j = 0; j < n - i - 1; j++) {
                 // Swap if the element found is greater than the next element
@@ -24,18 +27,24 @@ public class BubbleSort {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    swap++;
                 }
+            }
+            if(swap==0){
+                System.out.println("Array already sorted");
+                break;
+
             }
         }
     }
 
     // Helper method to print an array
-    public static void printArray(int[] arr) {
+   /* public static void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
-    }
+    }*/
 }
 
 
--- InsertionSort.java
+++ InsertionSort.java
@@ -3,13 +3,13 @@ public class InsertionSort {
         int[] A={40,20,30,50,10,60};
         int n=A.length;
         for (int j = 1; j <n; j++) {
-            int key=A[j];
-            int i=j-1;
-            while(i>=0 && A[i]>key){
-                A[i+1]=A[i];
-                i=i-1;
-            }
-            A[i+1]=key;
+            int current=A[j];
+            int prev=j-1;
+            while(prev>=0 && A[prev]>current){
+                A[prev+1]=A[prev];
+                prev=prev-1;
+            }   
+            A[prev+1]=current;
         }
       for (int i : A) {
          System.out.print(i+"\t");
