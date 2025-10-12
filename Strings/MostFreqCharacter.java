717a65ad34a2bde5cf6badc2f92065d98317af5a
Rumaisha02 <134322056+Rumaisha02@users.noreply.github.com>
2025-10-12 15:16:27 +0000

    executed

---
+++ Strings/MostFreqCharacter.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class MostFreqCharacter {
+    public static void main(String[] args) {
+        String s="minimalitst";
+        char st[]=s.toCharArray();
+        Arrays.sort(st);
+        
+}
+}
