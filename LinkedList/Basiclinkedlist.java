07b40678e8a3ad9638a57a2623df97bb6917c532
Rumaisha02 <134322056+Rumaisha02@users.noreply.github.com>
2025-10-21 09:39:31 +0000

    ..

--- LinkedList/Basiclinkedlist.java
+++ LinkedList/Basiclinkedlist.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class Basiclinkedlist{
     public class Node{
         int data;
         Node next;
+       
         public Node(int data){
             this.data=data;
             this.next=null;
@@ -9,10 +12,11 @@ public class Basiclinkedlist{
     }
       public static Node Head;
       public static Node Tail;
-      
+       public static int size;
       public void addFirst(int data){
         // create a node
          Node newNode=new Node(data);
+         size++;
              if(Head ==null){
                  Head=Tail=newNode;
              }
@@ -26,6 +30,7 @@ public class Basiclinkedlist{
       public void addLast(int data){
           // create a node
          Node newNode=new Node(data);
+         size++;
              if(Head ==null){
                  Head=Tail=newNode;
              }
@@ -35,7 +40,30 @@ public class Basiclinkedlist{
              //assign tail as the newNode
              Tail=newNode;
       }
-
+      public void addMiddle(int index,int data){
+         
+        
+      }
+      public int RemoveLast(){
+        if(size==0){
+            System.out.println("Linked List is empty");
+            return 0;
+        }else if(size==1){
+            int val=Head.data;
+            Head=Tail=null;
+            size=0;
+            return val;
+        }
+        Node prev=Head;
+        for( int i=0;i<size-2;i++){
+            prev=prev.next;
+        }
+        int val=prev.next.data;
+        prev.next=null;
+        Tail=prev;
+        size--;
+        return val;
+      }
       public void printLL( Node Head){
         Node temp=Head;
         while(temp!=null){
@@ -43,15 +71,36 @@ public class Basiclinkedlist{
             temp=temp.next;
         }
       }
+      public int search(int key){
+        Node temp=Head; int i=0;
+        while(temp!=null){
+            if(temp.data==key){
+            return i;
+            }
+            else{
+                temp=temp.next;i++;
+            }
+        }
+        return -1;
+      } 
 
 
 
     public static void main(String[] args){
+     Scanner sc=new Scanner(System.in);
      Basiclinkedlist ll=new Basiclinkedlist();
      ll.addFirst(1);
      ll.addFirst(2);
      ll.addLast(5);
      ll.addLast(3);
      ll.printLL(Head);
+     System.out.println(size);
+     System.out.println("Enter value to search: ");
+     int key=sc.nextInt();
+     int index=ll.search(key);
+     System.out.println("Key found at index node: "+ index);
+     int deleted=ll.RemoveLast();
+     System.out.println("node deleted="+deleted);
+      ll.printLL(Head);
     }
 }
